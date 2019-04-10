a4b33cbe82536b1f73948169a8d5448a2d31b088
Andres gualdron <gualdronsito@hotmail.com>
2019-04-09 19:41:47 -0500

    adelantando modelo de base de datos, e inicio de sesión

--- NGCM-app/src/main/java/com/proyectoNGCM/app/App.java
+++ NGCM-app/src/main/java/com/proyectoNGCM/app/App.java
@@ -1,13 +1,46 @@
 package com.proyectoNGCM.app;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	private static final transient Logger log = LoggerFactory.getLogger(App.class);
+	
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log.info("comenzando");
+        //1.
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        //2.
+        SecurityManager securityManager = factory.getInstance();
+
+        //3.
+        SecurityUtils.setSecurityManager(securityManager);
+        
+        Subject usuarioActual = SecurityUtils.getSubject();
+        Session session = usuarioActual.getSession();
+        if(!usuarioActual.isAuthenticated()) {
+        	System.out.println("no está autenticado");
+        	UsernamePasswordToken token = new UsernamePasswordToken("administrador","123");
+        	token.setRememberMe(true);
+        	usuarioActual.login(token);
+        	System.out.println("ya está autenticdo");
+        }
+        System.out.println(usuarioActual.isAuthenticated());
+        System.exit(0);
+        
     }
 }
