76374579f075742c20f479beeb7b865793168944
Andres Gualdron <gualdronsito@hotmail.com>
2019-04-11 09:40:05 -0500

    cierre de sesión terminado

--- sql/poblar.sql
+++ sql/poblar.sql
@@ -1,1 +1,1 @@
-insert into usuarios (nombre, correo, contrasena, carnet, rol) values ('Andres Gualdron','gualdronsito@hotmail.com','202cb962ac59075b964b07152d234b70',2128884,'admin');
+insert into usuarios (nombre, correo, contrasena, carnet, rol) values ('Andres Gualdron','gualdronsito@hotmail.com','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',2128884,'admin');
