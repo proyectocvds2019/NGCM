92c89c8dab4fa73ca72a6eb9ccf1f7a14b77adf0
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-04-11 17:46:43 -0500

    Implementación de Servicios Historial finalizado

--- src/main/java/edu/eci/cvds/samples/services/ServiciosHistorial.java
+++ src/main/java/edu/eci/cvds/samples/services/ServiciosHistorial.java
@@ -3,5 +3,7 @@ package edu.eci.cvds.samples.services;
 import edu.eci.cvds.samples.entities.Elemento;
 
 public interface ServiciosHistorial {
+	
 	public abstract void registrarElemento(Elemento elemento) throws ExcepcionServiciosHistorial;
+	
 }
