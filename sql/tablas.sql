957c05b8e670f9033cfd6956d7a15792cea488bf
Andres Gualdron <natalia.palacios-r@mail.escuelaing.edu.co>
2019-04-11 10:02:15 -0500

    Dejando en la base de datos solo lo necesario para el Sprint 1

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -8,7 +8,7 @@ CREATE TABLE Usuarios(
 	carnet INTEGER
 );
 
-CREATE SEQUENCE id_lab;
+/*CREATE SEQUENCE id_lab;
 CREATE TABLE Laboratorios(
 	id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('id_lab'),
 	nombre VARCHAR(100) NOT NULL
@@ -18,28 +18,28 @@ CREATE SEQUENCE id_equipo;
 CREATE TABLE Equipos(
 	id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('id_equipo'),
 	laboratorio INTEGER NOT NULL
-);
+);*/
 
 CREATE SEQUENCE id_elemento;
 CREATE TABLE Elementos(
 	id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('id_elemento'),
 	tipo VARCHAR(100) NOT NULL,
-	idEquipo INTEGER NOT NULL,
+	--idEquipo INTEGER NOT NULL,
 	registradoPor VARCHAR(100) NOT NULL
 );
 
 --- Foreigns
-ALTER TABLE Elementos ADD CONSTRAINT Elementos_Equipo
+/*ALTER TABLE Elementos ADD CONSTRAINT Elementos_Equipo
 	FOREIGN KEY (idEquipo)
-	REFERENCES Equipos (id);
+	REFERENCES Equipos (id);*/
 	
 ALTER TABLE Elementos ADD CONSTRAINT Elementos_Usuario
 	FOREIGN KEY (registradoPor)
 	REFERENCES Usuarios (correo);
 	
-ALTER TABLE Equipos ADD CONSTRAINT Equipo_Laboratorio
+/*ALTER TABLE Equipos ADD CONSTRAINT Equipo_Laboratorio
 	FOREIGN KEY (laboratorio)
-	REFERENCES Laboratorios (id);
+	REFERENCES Laboratorios (id);*/
 
 --- Checks
 
