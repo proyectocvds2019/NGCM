50389d03afff4e8732339ec5cfa364ea528dbc33
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-04-27 09:59:14 -0500

    Agregando a la base de datos las fechas

--- sql/xTablas.sql
+++ sql/xTablas.sql
@@ -1,13 +1,13 @@
 delete from elementos;
 delete from equipos;
---delete from laboratorios;
+delete from laboratorios;
 delete from usuarios;
 
 DROP TABLE Elementos;
 DROP TABLE Equipos;
---DROP TABLE Laboratorios;
+DROP TABLE Laboratorios;
 DROP TABLE Usuarios;
 
---DROP SEQUENCE id_lab;
+DROP SEQUENCE id_lab;
 DROP SEQUENCE id_equipo;
 DROP SEQUENCE id_elemento;
