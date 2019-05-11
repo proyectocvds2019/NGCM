a62ab48435bdf69ec21828c4266dfa2376eff0c5
AndresFelipeGualdron <gualdronsito@hotmail.com>
2019-05-11 12:02:16 -0500

    agregando entidad novedades

---
+++ src/main/java/edu/eci/cvds/samples/entities/Novedad.java
@@ -0,0 +1,71 @@
+package edu.eci.cvds.samples.entities;
+
+public class Novedad {
+    private Integer id;
+    private String titulo;
+    private String detalle;
+    private String usuario;
+    private String tipo;
+    private Elemento elemento;
+    private Equipo equipo;
+
+    public Novedad(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Elemento getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Elemento elemento) {
+        this.elemento = elemento;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
